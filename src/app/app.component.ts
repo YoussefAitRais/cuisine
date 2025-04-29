2a24383d6eea9d50e93915cf22e6e2d55228f7c5
Youssef Ait Rais <copybuisness007@gmail.com>
2025-04-29 16:55:44 +0200

    frontend setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, RecipeListComponent, RecipeDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
